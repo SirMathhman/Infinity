5daa469c72fa1e7e288e70b729eaeb77dbc59edd
SirMathhman <minecraftmathhman@gmail.com>
2019-06-25 13:48:05 -0400

    Remove bad code

--- src/main/java/com/meti/app/server/asset/AssetPropertiesRequest.java
+++
@@ -1,9 +0,0 @@
-package com.meti.app.server.asset;
-
-/**
- * @author SirMathhman
- * @version 0.0.0
- * @since 6/7/2019
- */
-public interface AssetPropertiesRequest {
-}
--- src/main/java/com/meti/lib/asset/source/ParentSource.java
+++
@@ -1,9 +0,0 @@
-package com.meti.lib.asset.source;
-
-/**
- * @author SirMathhman
- * @version 0.0.0
- * @since 6/6/2019
- */
-public interface ParentSource extends Source, Parent<Source> {
-}
