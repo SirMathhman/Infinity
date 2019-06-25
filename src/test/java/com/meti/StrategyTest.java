fa00f9c67fe56d27cba6868359d86bc3db8e9a2e
SirMathhman <minecraftmathhman@gmail.com>
2019-06-25 14:09:30 -0400

    Add StrategyTest

---
+++ src/test/java/com/meti/StrategyTest.java
@@ -0,0 +1,26 @@
+package com.meti;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class StrategyTest {
+    private Frame frame;
+
+    @BeforeEach
+    void before() {
+        this.frame = new Frame();
+        this.frame.setSize(600, 400);
+        this.frame.setTitle("Hello World");
+        this.frame.setVisible(true);
+    }
+
+    @Test
+    void strategyPresent() {
+        frame.createBufferStrategy(3);
+        assertNotNull(frame.getBufferStrategy());
+    }
+}
